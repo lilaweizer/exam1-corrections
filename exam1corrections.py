3ee5509564588bde3bde71764d964a350790ffd6
Lila Weizer <lilaweizer@uchicago.edu>
2024-02-23 08:55:36 -0600

    only correcting problem 3

---
+++ exam1corrections.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 23 08:50:56 2024
+
+@author: lilaw
+"""
+# Redo of Problem 3, Exam 1
+MONTH_DICT = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06', 'July': '07', 'August': '08', 'September': '09', 
+             'October': '10', 'November': '11', 'December': '12'}
+def format_helper(string):
+    formatted = string.split(" ")
+    formatted[1] = formatted[1].strip(",")
+    if len(formatted[1]) < 2:
+        formatted[1] = "0" + formatted[1]
+    formatted[0] = MONTH_DICT[formatted[0]]
+    
+    return formatted
+
+def dates_to_iso8601(dates):
+    iso_form = []
+    for d in dates:
+        components = format_helper(d)
+        iso = components[2] + "-" + components[0] + "-" + components[1]
+        iso_form.append(iso)
+    return iso_form
+
+def sort_dates(dates):
+    iso_list = dates_to_iso8601(dates)
+    date_dict = {}
+    for normal, iso in zip(dates, iso_list):
+        date_dict[iso] = normal
+    
+    sort1 = sorted(iso_list)
+    for day in sort1:
+        day = date_dict[day]
+    return sort1
+
+dates_list = ['February 6, 1992', 'February 8, 1992', 'December 1, 1993', 'January 1, 1990']
+test1 = sort_dates(dates_list)
